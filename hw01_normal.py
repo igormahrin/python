01f9397330127d5456760c9adbd1dd08bdd8f44c
igormahrin <iegnoyra@gmail.com>
2019-09-30 14:03:07 +0300

    fixed error

--- hw01_normal.py
+++ hw01_normal.py
@@ -50,8 +50,8 @@ if d == 0:
    x = -b/2*a
    print ('x =',x)
 if d > 0:
-    x1 = (-b + math.sqrt(d))/2*a
-    x2 = (-b - math.sqrt(d))/2*a
+    x1 = (-b + math.sqrt(d))/(2*a)
+    x2 = (-b - math.sqrt(d))/(2*a)
     print ('x1 =',x1,'x2 =',x2)
 if d < 0:
     print ('корней нет')
